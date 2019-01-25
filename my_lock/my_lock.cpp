4b01525e3a6d89d4aae94c018af1bf612f10bb7a
wenbin <928568891@qq.com>
2019-01-25 12:21:13 +0800

    Signed-off-by: wenbin <928568891@qq.com>

--- my_lock/my_lock.cpp
+++ my_lock/my_lock.cpp
@@ -1,11 +1,43 @@
 
 
 
+#include <pthread.h>
+#include <iostream>
+
+using namespace std;
+
+#define MUTEX_INIT_LOCK(mutex) \
+do { \
+	(void)pthread_mutex_init(&mutex, NULL); \
+}while(0)
+
+#define MUTEX_LOCK(mutex) \
+do { \
+	(void)pthread_mutex_lock(&mutex); \
+}while(0)
+
+#define MUTEX_UNLOCK(mutex) \
+do { \
+	(void)pthread_mutex_unlock(&mutex); \
+}while(0)
+
+#define MUTEX_DESTROY(mutex) \
+do { \
+	(void)pthread_mutex_destroy(&mutex); \
+}while(0)
 
 
 
 
+static pthread_mutex_t mutex;
 
+
+int fun()
+{
+	MUTEX_LOCK(mutex);
+	cout<<"fun"<<endl;	
+	MUTEX_UNLOCK(mutex);	
+}
 
 
 
@@ -21,5 +53,10 @@
 
 int main()
 {
+	MUTEX_INIT_LOCK(mutex);
+	
+	fun();
 	return 0;
 }
+
+
